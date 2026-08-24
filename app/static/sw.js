7ce17af6ffe653e224b5c4e826c830d1f1d6880b
aukh13 <abd.ulrahman.khaled@mni.thm.de>
2026-08-24 17:19:43 +0200

    Modernize profile menu with outline icons

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const VERSION = "sna-public-v5";
+const VERSION = "sna-public-v6";
 const SHELL = [
   "/", "/admin", "/manifest.webmanifest",
   "/static/style.css", "/static/map-views.css", "/static/coherence.css",
