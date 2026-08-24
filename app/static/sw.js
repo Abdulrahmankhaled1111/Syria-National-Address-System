93da502b7beda1808cdf21a0f960619de541d25a
aukh13 <abd.ulrahman.khaled@mni.thm.de>
2026-08-24 18:21:16 +0200

    Center and modernize map selector toggle

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const VERSION = "sna-public-v15";
+const VERSION = "sna-public-v16";
 const SHELL = [
   "/", "/admin", "/manifest.webmanifest",
   "/static/style.css", "/static/map-views.css", "/static/coherence.css",
