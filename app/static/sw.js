f013cc76687b2838f592ae55db3cdff64b40bf7c
aukh13 <abd.ulrahman.khaled@mni.thm.de>
2026-08-24 18:14:10 +0200

    Fix app search icon alignment

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const VERSION = "sna-public-v12";
+const VERSION = "sna-public-v13";
 const SHELL = [
   "/", "/admin", "/manifest.webmanifest",
   "/static/style.css", "/static/map-views.css", "/static/coherence.css",
