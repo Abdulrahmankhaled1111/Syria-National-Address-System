a2f968aeb0ef3883cb23971fad846fc37923a07a
aukh13 <abd.ulrahman.khaled@mni.thm.de>
2026-08-24 18:38:34 +0200

    Modernize staff geoportal map workspace

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const VERSION = "sna-public-v19";
+const VERSION = "sna-public-v20";
 const SHELL = [
   "/", "/admin", "/manifest.webmanifest",
   "/static/style.css", "/static/map-views.css", "/static/coherence.css",
