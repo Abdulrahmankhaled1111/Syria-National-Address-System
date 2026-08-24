1aa0bb889b47804259a9e8e847b2eb78ad6073c7
aukh13 <abd.ulrahman.khaled@mni.thm.de>
2026-08-24 17:49:05 +0200

    Split register workspace into specialist apps

--- app/static/sw.js
+++ app/static/sw.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const VERSION = "sna-public-v8";
+const VERSION = "sna-public-v9";
 const SHELL = [
   "/", "/admin", "/manifest.webmanifest",
   "/static/style.css", "/static/map-views.css", "/static/coherence.css",
